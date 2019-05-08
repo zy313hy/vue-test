75338e14c29c2dd357a69456e313f95a37b97e90
Eason <zy313hy@sina.com>
2019-05-08 14:40:17 +0800

    左侧导航

--- src/pages/store/goods/Goods.vue
+++ src/pages/store/goods/Goods.vue
@@ -1,13 +1,194 @@
 <template>
-  <div>goods</div>
+  <div>
+    <div class="goods">
+      <div class="menu-wrapper">
+        <ul ref="leftUl">
+          <li class="menu-item"  v-for="(good,index) in goods" :key="index"  :class="{current: currentIndex===index}">
+            <span class="text bottom-border-1px"><img class="icon" :src="good.icon" v-if="good.icon">{{good.name}}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="foods-wrapper">
+        <ul ref="rightUi">
+          <li class="food-list-hook" v-for="(good,index) in goods" :key="index">
+            <h1 class="title">{{good.name}}</h1>
+            <ul>
+              <li class="food-item bottom-border-1px" v-for="(food,index) in good.foods">
+                <div class="icon">
+                  <img width="57" height="57" :src="food.icon">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra" style="display: flex">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span></div>
+                  <div class="price" >
+                    <span class="now">￥{{food.price}}</span>
+                  </div>
+                  <div class="cartcontrol-wrapper">
+                    CartControl组件
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+  import {mapState} from 'vuex'
+  import BScroll from 'better-scroll'
   export default {
-    name: ""
+    data(){
+      return {
+        scrollY: 0, // 右侧列表滑动的坐标  在右侧滑动过程中更新此值
+        tops: [], // 右侧所有分类li的top值  在列表初始显示之后计算一次即可
+        food: {}, // 需要显示的当前food
+      }
+    },
+    computed:{
+      ...mapState(['goods']),
+      //当前分类列表
+      currentIndex(){
+       const {scrollY,tops}=this;
+       //找到匹配的下标
+        /*
+       findIndex(): 查找到第一个匹配元素的下标
+         [0, 4, 7, 15, 19]
+         7 8 14
+         scrollY>=top && scrollY<nextTop
+        */
+        const index=tops.findIndex((top,index)=>scrollY >= top && scrollY < tops[index + 1]);
+        return index
+      }
+    },
+   mounted(){
+
+   },
+    watch:{
+      goods(){
+        this.$nextTick(()=>{
+          new BScroll('.menu-wrapper',{click:true});
+          new BScroll('.foods-wrapper')
+        })
+      }
+    },
+    methods:{
+      initTop(){
+        const tops=[];
+        let height=0;
+        tops.push(height)
+        const lis=this.$refs.rightUi.children
+        Array.from(lis).forEach(li=>{
+          height+=li.clientHeight;
+          tops.push(height)
+        })
+        this.tops=tops;
+      }
+    }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
-
+  @import "../../../commom/stylus/mixins.styl"
+  .goods
+    display: flex
+    position: absolute
+    top: 225px
+    bottom: 46px
+    width: 100%
+    background: #fff;
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        padding: 0 12px
+        line-height: 14px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          color: $green
+          font-weight: 700
+          .text
+            border-none()
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          bottom-border-1px(rgba(7, 17, 27, 0.1))
+          font-size: 12px
+    .foods-wrapper
+      flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        bottom-border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            line-height: 12px
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 12px
 </style>
+
